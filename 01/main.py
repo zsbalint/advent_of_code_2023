b09411f21bdf46ddf98fbbf87d122a51958b99e5
zsbalint <zsolnaib@gmail.com>
2024-12-10 18:48:13 +0100

    1a is done

--- 01/main.py
+++ 01/main.py
@@ -1,9 +1,15 @@
+import re
+
 file = open('input.txt', 'r')
 raw_content = file.readlines()
 content = []
 # sorvégjelek törlése
 for item in raw_content:
     content.append(item.replace("\n", ""))
+sum = 0
+for row in content:
+    numbers = re.findall("\d", row)
+    result = numbers[0] + numbers[-1]
+    sum += int(result)
 
-
-print(content)
+print(sum)
