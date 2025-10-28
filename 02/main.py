d243ffa557ec45db431690b769fd54c3d724e123
zsbalint <zsolnaib@gmail.com>
2025-10-28 20:43:24 +0100

    2a is done

---
+++ 02/main.py
@@ -0,0 +1,36 @@
+RED = 12
+GREEN = 13
+BLUE = 14
+
+file = open('input.txt', 'r')
+raw_content = file.readlines()
+content = {}
+for item in raw_content:
+    item = item.replace('\n', '')
+    key = item.split(':')[0]
+    key = key[5:]
+    values = item.split(':')
+    values.pop(0)
+    values = values[0].split(';')
+    all_shows = []
+    for v in values:
+        show_dict = {'green': '0', 'red': '0', 'blue':'0'}
+        current_round = []
+        current_round.append(v.split(','))
+        for show in current_round[0]:
+            number, color = show.lstrip().split(' ')
+            show_dict[color] = number
+        all_shows.append(show_dict)
+    content[key] = all_shows
+
+
+sum = 0
+
+for key, value in content.items():
+    correct = True
+    for showing in value:
+        if int(showing['red']) > RED or int(showing['green']) > GREEN or int(showing['blue']) > BLUE:
+            correct = False
+    if correct:
+        sum += int(key)
+print(sum)
